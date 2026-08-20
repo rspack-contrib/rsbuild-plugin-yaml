3857514dd6c378e15da56b2ec6e3bdbba170b87c
Jiahan Chen <chenjiahan.jait@bytedance.com>
2026-08-20 18:37:02 +0800

    chore(deps): upgrade Rslint to v0.8.1 (#74)

--- rslint.config.ts
+++ rslint.config.ts
@@ -1,3 +1,24 @@
-import { defineConfig, ts } from '@rslint/core';
+import { defineConfig, globals, js, ts } from '@rslint/core';
 
-export default defineConfig([ts.configs.recommended]);
+export default defineConfig([
+  js.configs.recommended,
+  ts.configs.recommended,
+  {
+    files: ['playground/src/**/*'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  {
+    files: ['**/*.test.{ts,tsx}'],
+    languageOptions: {
+      globals: globals.rstest,
+    },
+  },
+  {
+    files: ['test/**/src/**/*.{js,jsx}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+]);
